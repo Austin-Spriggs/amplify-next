d62e5e0c8b897aff313be94bc0bb4e9d4753163b
Austin-Spriggs <aspriggs@madisoncollege.edu>
2026-05-24 22:14:46 -0500

    idk why but this works better so lets not touch it

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -1,14 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-	try {
-		const response = await fetch('https://apip.cc/json');
-		const data = await response.json();
+	const response = await fetch('https://apip.cc/json');
+	const data = await response.json();
 
-		return NextResponse.json(data);
-	} catch (e) {
-		console.error(e);
-
-		return NextResponse.json({ error: 'Request failed.'});
-	}
+	return NextResponse.json(data);
 }
